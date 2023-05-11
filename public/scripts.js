a6f0aa0f44641ef96a5ce24f6e270dd884a19caa
Xiaohan Yu <91620376+Reaganuu@users.noreply.github.com>
2023-05-11 06:16:24 -0400

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,30 +1,18 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
 // If you would like to see some examples of similar code to make an interface interact with an API,
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
-function play() {
-  var playrps = document.getElementById("rps");
+function oppcheck() {
   var playrpsls = document.getElementById("rpsls");
-  var opp = document.getElementById("opponent");
-  if (playrps.checked == true) {
-    if (opp.checked == true) {
-      console.log("rps+opp");
-      alert("rps+opp");
-    } else {
-      console.log("rps");
-      alert("rps");
-    }
-  } else if (playrpsls.checked == true) {
-    if (opp.checked == true) {
-      console.log("rpsls+opp");
-      alert("rpsls+opp");
-    } else {
-      console.log("rpsls");
-      alert("rpsls");
-    }
-  } else {
-    alert("Please choose the game!");
+  var oppchoice = '<br><input type="radio" name="oppOpt" id="rpsOpt">Rock</br>';
+  oppchoice += '<br><input type="radio" name="oppOpt" id="rpsOpt">Paper</br>';
+  oppchoice +=
+    '<br><input type="radio" name="oppOpt" id="rpsOpt">Scissors</br>';
+
+  if (playrpsls.checked == true) {
+    oppchoice +=
+      '<br><input type="radio" name="oppOpt" id="rpsOpt">Lizard</br>';
+    oppchoice += '<br><input type="radio" name="oppOpt" id="rpsOpt">Spock</br>';
   }
+  document.getElementById("oppopt").innerHTML = oppchoice;
+  //alert("have opp");
 }
